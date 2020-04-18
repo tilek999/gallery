56e1f6ddc35043c43ce5a331147e7e62c562c480
tilek999 <58734791+tilek999@users.noreply.github.com>
2020-04-18 21:10:12 +0600

    changed server

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
 from flask import Flask, render_template, request
+from openpyxl import load_workbook
 
 
 app=Flask(__name__)
 
 @app.route("/")
 def gallery():
-    gal = open("gallery.txt", "r", encoding="utf-8")
-    gal.list = [row for row in gal]
-    gal.close
-    return render_template("gallery.html" ,gal=gal)
+    l = []
+    excel = load_workbook("gallery.xlsx")
+    page = excel["Sheet1"]
+    for row in page:
+        g = row[0].value
+        c = row[1].value
+        title = row[2].value
+        lst = [title, g, c]
+        l.append(lst)
+
+    return render_template("gallery.html" ,l=l)
 
 @app.route("/add-picture", methods=["POST"])
 def add_picture():
     g = request.form.get("g")
-    gal = open('gallery.txt', 'a+', encoding="utf-8")
-    gal.write( g + '\n' )
-    gal.close()
+    c = request.form.get("comment")
+    title = request.form.get("title")
+    
+    excel = load_workbook("gallery.xlsx")
+    sheet = excel["sheet1"]
+    sheet.append(title, g, c)
+    excel.save("gallery.xlsx")
     return render_template("gallery.html")
 @app.route("/ad")
 def add():
